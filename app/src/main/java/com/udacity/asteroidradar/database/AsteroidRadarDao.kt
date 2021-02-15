7628ae730f0a2c060e04bd0b98e5db2703b20147
Parzival <parzival.nieminen@gmail.com>
2021-02-15 03:05:45 +0100

    feat: first Database setup for Android Radar
    * add Dao interface
    * add Asteroid.kt entity
    * add AsteroidRadarApplication.kt
    * add Timber dependency
    * add Timber to AndroidManifest.xml

--- app/src/main/java/com/udacity/asteroidradar/database/AsteroidRadarDao.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/AsteroidRadarDao.kt
@@ -1,9 +1,11 @@
 package com.udacity.asteroidradar.database
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 
+@Dao
 interface AsteroidRadarDao {
 
     @Insert
