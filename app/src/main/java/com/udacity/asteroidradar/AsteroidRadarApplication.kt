153b05eff8408d74ba68a719330ac7a86141a12f
Parzival <parzival.nieminen@gmail.com>
2021-02-16 01:54:28 +0100

    feat add recyclerView asteroid
    * add AsteroidAdapter.kt
    * add AsteroidAdapterDiffCallback.kt
    * add empty layout list_item_asteroid.xml as placeholder
    * fix line space to end of file

--- app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
+++ app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
@@ -3,7 +3,7 @@ package com.udacity.asteroidradar
 import android.app.Application
 import timber.log.Timber
 
-class AsteroidRadarApplication: Application() {
+class AsteroidRadarApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
